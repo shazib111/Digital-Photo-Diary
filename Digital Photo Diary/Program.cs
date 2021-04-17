1bd3aa0b7d18a651d9254e446dcd8df36c07309c
MD.SHAZIB SHEIKH <66199263+shazib111@users.noreply.github.com>
2021-04-18 02:25:25 +0600

    Update Program.cs

--- Digital Photo Diary/Program.cs
+++ Digital Photo Diary/Program.cs
@@ -18,6 +18,7 @@ namespace Digital_Photo_Diary
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Digital_Photo_Diary());
         }
     }
 }
